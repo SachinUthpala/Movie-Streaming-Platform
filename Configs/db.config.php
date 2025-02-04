d1001e5e7f768894a7e4de33473b45271741779e
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2025-02-04 21:27:52 +0530

    db connection completed

---
+++ Configs/db.config.php
@@ -0,0 +1,11 @@
+<?php
+$ServerName = "localhost";
+$UserName = "root";
+$Password = "";
+$DbName = "moviehub";
+
+$conn = new mysqli($ServerName, $UserName, $Password, $DbName);
+
+if ($conn->connect_error) {
+    echo "connection Fail";
+}
